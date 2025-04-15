62d5bb57d765ff68e85ade6af16d0e8a33618340
Serhii-Bohatkin <131189925+Serhii-Bohatkin@users.noreply.github.com>
2025-04-15 14:10:04 +0300

    Added book entity, controller, service, repository and mapper for it. Added liquibase support (#1)

---
+++ src/main/java/org/example/bookstore/controller/BookController.java
@@ -0,0 +1,51 @@
+package org.example.bookstore.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.bookstore.dto.BookDto;
+import org.example.bookstore.dto.CreateBookRequestDto;
+import org.example.bookstore.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("books")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping
+    public List<BookDto> getAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public BookDto getBookById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+        return bookService.save(bookDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@RequestBody CreateBookRequestDto requestDto, @PathVariable Long id) {
+        return bookService.update(requestDto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
+}
---
+++ src/main/java/org/example/bookstore/mapper/BookMapper.java
@@ -0,0 +1,17 @@
+package org.example.bookstore.mapper;
+
+import org.example.bookstore.config.MapperConfig;
+import org.example.bookstore.dto.BookDto;
+import org.example.bookstore.dto.CreateBookRequestDto;
+import org.example.bookstore.model.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface BookMapper {
+    BookDto toDto(Book book);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    Book toModel(CreateBookRequestDto bookDto);
+}
